b8225eb14b8f73e23c3cb643f31f83e816b0d8c2
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-01-30 23:59:35 +0200

    Top section with slider complete

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        asNavFor: '.slider__menu'
+    });
+    $('.slider__menu').slick({
+        asNavFor: '.slider__inner',
+        slidesToShow: 6,
+        focusOnSelect: true
+    });
+});
