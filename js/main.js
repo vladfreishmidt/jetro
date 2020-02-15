607ca85474e98f28294e7931289854e25868ae2d
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-02-15 12:45:32 +0200

    Added portfolio and works ssections

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: false,
-        asNavFor: '.slider__menu'
+        asNavFor: '.slider__menu',
+
     });
     $('.slider__menu').slick({
         asNavFor: '.slider__inner',
         slidesToShow: 6,
-        focusOnSelect: true
+        focusOnSelect: true,
+        infinite: true
     });
 });
